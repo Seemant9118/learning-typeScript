13b73cd712db28a619402dd5e7b23a9e2614e031
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-11-28 12:23:54 +0530

    intro of types, syntax, situation where to use.

---
+++ types.js
@@ -0,0 +1,9 @@
+// types availabled in typescrip : Number, String, Boolean, Null, Undefined, Void, Object, Array, Tuples, Any, Never, Unknown and more
+// syntax
+// For variable: let variableName:type = value;
+var greetings = "Hello, Seemant";
+console.log(greetings);
+// situations
+// 1. A function accepts two numbers
+// 2. A function returns a string
+// 3. 
---
+++ types.ts
@@ -0,0 +1,15 @@
+// types availabled in typescrip : Number, String, Boolean, Null, Undefined, Void, Object, Array, Tuples, Any, Never, Unknown and more
+
+// situations where we use ts
+// 1. A function accepts two numbers
+// 2. A function returns a string
+
+// syntax
+// For variable: let variableName:type = value;
+let greetings: string = "Hello, Seemant";
+console.log(greetings);
+
+
+
+
+
