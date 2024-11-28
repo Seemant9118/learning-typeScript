362106a2441dd77009185522af4d20f8278cff02
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-11-28 12:05:06 +0530

    transpiled intro.ts into intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+console.log("Seemant");
+var user = {
+    name: 'seemant',
+    age: 20,
+    email: 'seemant@gmail.com'
+};
+console.log(user.re);
---
+++ intro.ts
@@ -0,0 +1,11 @@
+// we create .ts file and write ts code
+// but if we run that file using tsc intro.ts then it transpiled into intro.js
+// it means, ts is a just a development tool actually all code runs in js
+
+let user = {
+    name: 'seemant',
+    age: 20,
+    email: 'seemant@gmail.com'
+}
+
+console.log(user.name);
