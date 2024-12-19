8385b18bf8ffec06c842cbecc2cb9165f33bcc8a
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-12-19 16:40:32 +0530

    type aliases

--- ObjectsInTypeScript.ts
+++ ObjectsInTypeScript.ts
@@ -5,15 +5,29 @@ const User = {
 }
 
 // Bad behaviour of objects in typescript
-function createUser({ name: string, isPaid: boolean }) {
+function createUsers({ name: string, isPaid: boolean }) {
 
 }
 let newUser = { name: "seemant", isPaid: false, email: "seem@gmail.com" }
-createUser(newUser)
+createUsers(newUser)
 
 // little bit confusing that return type of function is object with litrals type annotations
 function createCourse(): { name: string, price: number } {
     return { name: "react.js", price: 499 }
 }
+
+
+// Type Alias
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean,
+}
+// function decleration with type alias
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true };
+}
+// function call with type alias
+createUser({ name: "", email: "", isActive: true });
 
 export { }
